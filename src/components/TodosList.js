0d13eb7d71611567ccb139e8c661e9b9d4de215b
Dominik Cebula <dominikcebula@gmail.com>
2024-05-08 20:05:31 +0200

    Adding reducer and extract components

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,49 +1,12 @@
 import * as React from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
+import TodoListItem from "./TodoListItem";
 
 export default function TodosList({ todoItems }) {
   return (
     <List>
       {todoItems.map((todoItem) => {
-        const labelId = `todo-item-${todoItem.id}`;
-
-        return (
-          <ListItem
-            key={todoItem.id}
-            secondaryAction={
-              <>
-                <IconButton edge="end" aria-label="edit">
-                  <EditIcon />
-                </IconButton>
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todoItem.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={todoItem.name} />
-            </ListItemButton>
-          </ListItem>
-        );
+        return <TodoListItem todoItem={todoItem} />;
       })}
     </List>
   );
